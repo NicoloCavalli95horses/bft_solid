975e64a06e7fd4e23df1bbbcddea7307c1b44d3b
Nicolò Cavalli <nicolo.cavalli95@gmail.com>
2025-09-29 14:32:00 +0200

    removed bdt from prod

--- app.config.js
+++ app.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from "@solidjs/start/config";
 import solid from 'vite-plugin-solid';
-import devtools from 'solid-devtools/vite';
+// import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
   plugins: [
-    devtools({
-      /* features options - all disabled by default */
-      autoname: true, // e.g. enable autoname
-    }),
+    // devtools({
+    //   /* features options - all disabled by default */
+    //   autoname: false, // e.g. enable autoname
+    // }),
     solid(),
   ],
 })
--- src/entry-client.jsx
+++ src/entry-client.jsx
@@ -1,5 +1,5 @@
 // @refresh reload
 import { mount, StartClient } from "@solidjs/start/client";
-import 'solid-devtools/setup';
+// import 'solid-devtools/setup';
 
 mount(() => <StartClient />, document.getElementById("app"));
