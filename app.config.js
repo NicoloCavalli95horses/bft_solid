7841bb066608a06fdd9d21bec9a2ab2280e56d13
Nicolò Cavalli <nicolo.cavalli95@gmail.com>
2025-10-08 10:30:18 +0200

    fix

--- app.config.js
+++ app.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "@solidjs/start/config";
 import solid from 'vite-plugin-solid';
-// import devtools from 'solid-devtools/vite';
+import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
   plugins: [
